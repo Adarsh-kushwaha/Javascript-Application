be74d548723be5c03159a14bb5ed84afe679b136
adarsh <adarshkushwaha296@gmail.com>
2021-08-10 09:56:10 +0530

    popup project and slider

---
+++ Slider/main.js
@@ -0,0 +1,31 @@
+
+ var btn2 = document.getElementById("btn1");
+ var slide = document.getElementsByClassName("slide");
+
+ 
+
+ const slide1=()=>{
+    slide[0].style.transform = "translateX(-600px)"
+ }
+ 
+ btn2.addEventListener("click", slide1);
+
+
+
+
+
+    // btn[0].onclick = function () {
+    //     slide[0].style.transform = "translateX(0px)"
+    // }
+    
+    // btn[1].onclick = function () {
+    //     slide[1].style.transform = "translateX(-600px)"
+    // }
+    // btn[2].onclick = function () {
+    //     slide[2].style.transform = "translateX(-1200px)"
+    // }
+
+
+
+
+
